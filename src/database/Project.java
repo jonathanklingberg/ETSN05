700ca04b528c71e40da5c92a9b5d03c335a8e686
Johan Malmgren <malmgren.jo@gmail.com>
2014-10-01 16:11:31 +0200

    Started implementing Project skeleton

--- src/database/Project.java
+++ src/database/Project.java
@@ -1,7 +1,49 @@
 package database;
 
-public class Project implements DatabaseInterface {
+import java.sql.Connection;
+import java.util.List;
 
+public class Project implements DatabaseInterface {
+	private int id;
+	private String name;
+	private Connection conn;
+	
+	public Project(Connection conn, int id, String name) {
+		
+	}
+	
+	public List<User> getUsers() {
+		return null;
+	}
+	
+	public User getUser(String userName) {
+		return null;
+	}
+	
+	public List<TimeReport> getTimeReports() {
+		return null;
+	}
+	
+	public boolean addTimeReport(TimeReport report) {
+		return false;
+	}
+	
+	public boolean removeTimeReport(TimeReport report) {
+		return false;
+	}
+	
+	public boolean signTimeReport(User signer, int reportId) {
+		return false;
+	}
+	
+	public boolean addUser(User user) {
+		return false;
+	}
+	
+	public boolean removeUser(User use) {
+		return false;
+	}
+	
 	@Override
 	public String toHTML(User user) {
 		// TODO Auto-generated method stub
