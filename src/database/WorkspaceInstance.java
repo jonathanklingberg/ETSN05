19cb45119dfa6904f0147d2eef7ecdd9ddef716f
Johan Malmgren <malmgren.jo@gmail.com>
2014-10-03 16:25:59 +0200

    Added synchronized in WorkspaceInstance

--- src/database/WorkspaceInstance.java
+++ src/database/WorkspaceInstance.java
@@ -33,7 +33,7 @@ public class WorkspaceInstance implements DatabaseInterface {
  	* 
  	* @return The projects associated with this workspace
  	*/
-	public List<Project> getProjects() {
+	public synchronized List<Project> getProjects() {
 		return null;
 	}
 
@@ -42,7 +42,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param project The project to add to the workspace.
 	 * @return True if it succeeds adding the project, otherwise false.
 	 */
-	public boolean addProject(Project project) {
+	public synchronized boolean addProject(Project project) {
 		return false;
 	}
 
@@ -51,7 +51,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param project The project to be removed from the workspace.
 	 * @return True if it succeeds with removing the project, otherwise false.
 	 */
-	public boolean removeProject(Project project) {
+	public synchronized boolean removeProject(Project project) {
 		return false;
 	}
 
@@ -59,18 +59,18 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * 
 	 * @param user The user to be added to the database
 	 */
-	public void addUser(User user) {}	
+	public synchronized void addUser(User user) {}	
 	/**
 	 * 
 	 * @param user The user to be removed from the database
 	 */
-	public void removeUser(User user) {}
+	public synchronized void removeUser(User user) {}
 	
 	/**
 	 * 
 	 * @return A list of users in the workspace
 	 */
-	public List<User> getUsers() {
+	public synchronized  List<User> getUsers() {
 		return null;
 	}
 
@@ -79,7 +79,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param id The id of the project to retrieve
 	 * @return The Project that maps to id in the database
 	 */
-	public Project getProject(int id) {
+	public synchronized Project getProject(int id) {
 		return null;
 	}
 	
@@ -89,7 +89,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param project1 The project the user should be moved from
 	 * @param project2 The project the user should be moved to
 	 */
-	public void moveUser(User user, Project project1, Project project2) {}
+	public synchronized void moveUser(User user, Project project1, Project project2) {}
 	
 	/**
 	 * 
@@ -97,7 +97,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param role The role to change to.
 	 * @return True if it succeeds, false otherwise.
 	 */
-	public boolean changeRole(User user, int role) {
+	public synchronized boolean changeRole(User user, int role) {
 		return false;
 	}
 	
@@ -107,7 +107,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param name The new user name
 	 * @return True if it succeeds, false otherwise
 	 */
-	public boolean changeUserName(User user, String name) {
+	public synchronized boolean changeUserName(User user, String name) {
 		return false;
 	}
 	
@@ -117,7 +117,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param password The new password
 	 * @return True if it succeeds, false otherwise.
 	 */
-	public boolean changePassword(User user, String password) {
+	public synchronized boolean changePassword(User user, String password) {
 		return false;
 	}
 	
@@ -125,7 +125,7 @@ public class WorkspaceInstance implements DatabaseInterface {
 	 * @param user The user who wants to print a project.
 	 * @return Returns the user in HTML representation.
 	 */
-	public String toHTML(User user) {
+	public synchronized String toHTML(User user) {
 		return null;
 	}
 
