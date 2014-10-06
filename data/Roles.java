388385628bebf66ade1dd220d8744752d3530013
sebbem3 <sebastian.mellstrom@gmail.com>
2014-10-06 18:10:49 +0200

    Added enum Types and enum Roles

---
+++ data/Roles.java
@@ -0,0 +1,5 @@
+package data;
+
+public enum Roles {
+	SystemArchitect, Developer, Tester, ProjectManager, Unspecified;
+};
---
+++ data/Types.java
@@ -0,0 +1,17 @@
+package data;
+
+
+
+public enum Types {
+    SDP(11), SRS(12), SVVS(13), STLDD(14), SVVI(15), SDDD(16), SVVR(17), SSD(18), 
+    ProjectFinalReport(19), FunctionalTesting(21), SystemTesting(22), RegressionTesting(23), Meeting(30),
+    Lecture(41), Exercise(42), ComputerExercise(43), OutsideReading(44), Other(100);
+    private int value;
+
+    private Types(int value) {
+            this.value = value;
+    }
+    public int getValue() {
+    	return this.value;
+    }
+};
