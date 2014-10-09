eeb02f2bc5f7bb8139871c048853a6192c1cfe21
NickSjo <niklas.c.sjoberg@gmail.com>
2014-10-09 09:40:15 +0200

    Started implementing doGet in WorkerComponent.

--- src/base/WorkerComponent.java
+++ src/base/WorkerComponent.java
@@ -35,24 +35,41 @@ public class WorkerComponent extends ServletBase {
 	  * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	  */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				PrintWriter out = response.getWriter();
-				out.println(getPageIntro());
+		PrintWriter out = response.getWriter();
+		out.println(getPageIntro());
 
-				// Get the session
-				HttpSession session = request.getSession(true);
+		// Get session and username
+		HttpSession session = request.getSession(true);
+		String userName = (String) session.getAttribute("name");
+		
 				
+		if (!isLoggedIn(request)) {
+			response.sendRedirect("logincomponent");
+		} else {
+			if (!WorkspaceInstance.getInstance(conn).userIsProjectManager(userName) && !userName.equals("admin")) { // Check that the user is a "default user"
+				out.println("<h1> Workercomponent " + "<h1>");
 				
-				
-				String userName = (String) session.getAttribute("name");
-
-				
+				//Prints username and project group ID
 				long projectGroup = WorkspaceInstance.getInstance(conn).getUser(userName).getGroupId();
-				
-								
 				out.println("<p> Logged in as: " + userName + " </p>");
+				out.println("<p> Assigned to project group: " + projectGroup + " </p>");
 				
-				out.println("<p> Assigned to project: project group" + projectGroup + " </p>");
-
+				//Display all project members in project group
+				//Anropa en metod för att hämta alla medlemmar i en viss grupp. Metoden bör skapas i WorkspaceInstance 
+				out.println("<p>Group members</p>");
+				out.println("<TABLE BORDER=1 CELLPADDING=0 CELLSPACING=0 WIDTH=70%>");
+				out.println("<tr><td><CENTER><B>NAME</B></CENTER></td>");
+				out.println("<tr><td><CENTER><B>ROLE</B></CENTER></td>");
+//				for(int i = 0; i < groupMembers.length(); i++){
+//					out.println("<tr><td><CENTER>" + " "  + "</CENTER></td>");
+//					out.println("<tr><td><CENTER>" + " "  + "</CENTER></td>");
+//				}
+				
+				
+			} else {
+					
+			}
+		}
 	}
 	
 
