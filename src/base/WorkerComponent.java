3b7d09b95ffc334f0bb19e25953e613d6861f0c3
Filip Svensson <adi10fsv@student.lu.se>
2014-10-16 15:45:52 +0200

    change code that checks if a time report is signed while editing

--- src/base/WorkerComponent.java
+++ src/base/WorkerComponent.java
@@ -213,7 +213,7 @@ public class WorkerComponent extends ServletBase {
 									instance.addTimeReport(new TimeReport(userId, currentUser.getGroupId(), type, 
 											duration, week, Date.valueOf(date), false, number));
 								}
-								resultMsg = existingReport ? "<pclass=\"success-message\">Time report was edited successfully!</p>" : "<p class=\"success-message\">Time report was created successfully!</p>";
+//								resultMsg = existingReport ? "<pclass=\"success-message\">Time report was edited successfully!</p>" : "<p class=\"success-message\">Time report was created successfully!</p>";
 							} else {
 								resultMsg = "<p class=\"failure-message\">Wrong format on input! Please try again!</p>";
 							}
