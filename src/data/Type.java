c4906c974c3e821b6f0088024f71a40da90048e5
FarhadJohari <johari.farhad@gmail.com>
2014-10-22 13:35:48 +0200

    Java doc for the enum class Type added.

--- src/data/Type.java
+++ src/data/Type.java
@@ -6,15 +6,31 @@ package data;
  * @author SG
  * @version 0.2
  */
-//TODO JavaDoc
+
 public enum Type {
     Development('D'), Formal('F'), Informal('I'), Rework('R');
     private char value;
-
+    
+    /***
+     * The constructor of the enum class. It takes an char value as
+     * parameter to represent a type.
+     * 
+     * @param a char value representing a type.
+     */
     private Type(char value) {
     	this.value = value;
     }
     
+    /***
+     * This method checks if the given char in the parameters is a valid type.
+     * It loops through the different enum values and checks if one of them equals the
+     * char given through the input. If a match is found it sets a boolean value to true
+     * and returns it, otherwise it returns the boolean value false.
+     * 
+     * @param charType represents the type that is going to be checked.
+     * @return true if the given input char represents a type,
+     * 			false otherwise.
+     */
     public static boolean isType(char charType){
     	boolean isExistingType = false;
     	for (Type t : Type.values()){
@@ -25,6 +41,10 @@ public enum Type {
     	return isExistingType;
     }
     
+    /***
+     * A getter returning the char value of the type.
+     * @return a char that represents the type.
+     */
     public char getValue() {
     	return this.value;
     }
